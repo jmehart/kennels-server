22b31c2fe0b5bce091fc62d3e0b1cffac09c9331
Jaimie Hart <hartjme@gmail.com>
2022-04-08 12:50:07 -0400

    On to Chapter 11

--- views/__init__.py
+++ views/__init__.py
@@ -1,7 +1,7 @@
-from .animal_requests import get_all_animals, get_single_animal
+from .animal_requests import get_all_animals, get_single_animal, get_animals_by_location_id, get_animals_by_status
 
 from .location_requests import get_all_locations, get_single_location
 
-from .employee_requests import get_all_employees, get_single_employee
+from .employee_requests import get_all_employees, get_single_employee, get_employees_by_location_id
 
-from .customer_requests import get_all_customers, get_single_customer
+from .customer_requests import get_all_customers, get_single_customer, get_customers_by_email
--- views/employee_requests.py
+++ views/employee_requests.py
@@ -66,6 +66,33 @@ def get_single_employee(id):
         employee = Employee(data['id'], data['name'], data['address'], data['location_id'])
 
         return json.dumps(employee.__dict__)
+    
+    
+def get_employees_by_location_id(location_id):
+
+    with sqlite3.connect("./kennel.sqlite3") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        # Write the SQL query to get the information you want
+        db_cursor.execute("""
+        select
+            a.id,
+            a.name,
+            a.address,
+            a.location_id
+        from Employee a
+        WHERE a.location_id = ?
+        """, ( location_id, ))
+
+        employees = []
+        dataset = db_cursor.fetchall()
+
+        for row in dataset:
+            employee = Employee(row['id'], row['name'], row['address'], row['location_id'] )
+            employees.append(employee.__dict__)
+
+    return json.dumps(employees)       
 
 
 # EMPLOYEES = [
